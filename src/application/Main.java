9b204555d91f66ffa8d9990a766aa33c08b2d1f9
anderson-indiano <indianobox2@gmail.com>
2021-04-22 20:35:19 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,33 +1,39 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+
+	private static Scene mainScene;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			
+
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			
-			Scene mainScene = new Scene(scrollPane);
+
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
